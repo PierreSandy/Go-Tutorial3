5eac300ba3317b52dff12736d9fcf7d157951f33
Pierre Sandy <pierre.sandy@9spokes.com>
2022-04-25 10:14:15 +1200

    if else statements

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//IF ELSE STATEMENTS
+
+// func main() {
+// 	var s = "Japan"
+// 	x := true
+// 	if x {
+// 		fmt.Println(s)
+// 	}
+// }
+
+
+
+// func main () {
+// 	x := 100
+
+// 	if x == 100{
+// 	fmt.Println("New York")
+// }else {
+// 	fmt.Println("Auckland")
+// }
+// }
+
+// MULTIPLE IF ELSE STATEMENTS
+
+// func main () {
+// 	x := 75
+
+// 	if x == 50 {
+// 		fmt.Println("New York")
+// 	} else if x == 100{
+// 		fmt.Println("London")
+//  	} else {
+// 		 fmt.Println("Miami")
+// 	 }
+// }
+
+// IF ELSE STATEMENT INITIALIZATION
+
+// func main () {
+// 	if x :=100; x == 100{
+// 		fmt.Println("Scotia")
+// 	}
+// }
+
+// func main () {
+// 	today :=
+// }
